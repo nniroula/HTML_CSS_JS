00025fd8beae8af46835436adf47250ed9700ca0
nabin <nkbhaie2017@gmail.com>
2021-12-17 16:16:36 -0700

    add js file for practice

---
+++ app.js
@@ -0,0 +1,9 @@
+let first = "Happy Coding";
+console.log(first)
+// alert("HEllo")
+
+function programming(){
+    let name = "Springboard Bootcamp";
+    return name
+}
+// programming()  you can call this in console
