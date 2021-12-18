3a31f7f0f8578b046db502ef9dc5cb9c9fbdb002
nabin <nkbhaie2017@gmail.com>
2021-12-17 20:13:04 -0700

    add app.js file

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-let first = "Happy Coding";
-console.log(first)
-// alert("HEllo")
+// let first = "Happy Coding";
+// console.log(first)
+// // alert("HEllo")
 
-function programming(){
-    let name = "Springboard Bootcamp";
-    return name
-}
+// function programming(){
+//     let name = "Springboard Bootcamp";
+//     return name
+// }
 // programming()  you can call this in console
+
+// try catch for error handling
+// try{
+//     apple();
+// }
+// // catch {
+// //     console.log("error happened")
+// // }
+// catch(e) {
+//     console.log(e);
+// }
+
+// First class function- a function that can be stored in a variable, that can be used in other function as arugment, and that can be returend too.
+
+
+function hello(){
+    console.log("Hello coding world")
+}
+
+function callAnotherFunc(sayHi){
+    sayHi();
+    sayHi();
+    sayHi();
+    sayHi();
+    sayHi();
+    sayHi();
+
+}
+
+// callAnotherFunc(hello);   // you can call this in console
